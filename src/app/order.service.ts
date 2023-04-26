23f7ad448ff80830d82add3430e8fab1817c32e5
rafasdiass <rafaeldias85@live.com>
2023-04-26 19:39:27 -0300

    header

--- src/app/order.service.ts
+++ src/app/order.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -11,6 +11,10 @@ export class OrderService {
   constructor(private http: HttpClient) {}
 
   createOrder(orderData: any): Observable<any> {
-    return this.http.post<any>(`${this.apiUrl}/ordens`, orderData);
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer ' + localStorage.getItem('token')
+    });
+
+    return this.http.post<any>(`${this.apiUrl}/orders`, orderData, { headers });
   }
 }
