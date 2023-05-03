d97fb1d2e36f67e9c5031489fb393b0e6dde7144
rafasdiass <rafaeldias85@live.com>
2023-05-03 15:45:09 -0300

    rota

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -11,12 +11,7 @@ import { ProductService } from '../services/product.service';
 })
 export class ItemListComponent implements OnInit {
   items: Item[] = [
-    new Item(1, 'Bolo de Chocolate', '', 15.99, 'assets/img/doce1.jpeg', 0),
-    new Item(2, 'Torta de Limão', '', 12.99, 'assets/img/doce2.jpeg', 0),
-    new Item(3, 'Cheesecake de Morango', '', 18.99, 'assets/img/doce3.jpeg', 0),
-    new Item(4, 'Pavê de Amendoim', '', 14.99, 'assets/img/doce4.jpeg', 0),
-    new Item(5, 'Tiramisu', '', 19.99, 'assets/img/doce5.jpeg', 0),
-    new Item(6, 'Tiramisu', '', 19.99, 'assets/img/doce6.jpeg', 0)
+    // ... lista de itens ...
   ];
 
   constructor(
@@ -38,7 +33,17 @@ export class ItemListComponent implements OnInit {
   }
 
   addItem(item: Item, quantity: number) {
+    // Adicione o produto ao carrinho usando o serviço CartService (localmente)
     this.cartService.addItem(item, quantity);
+
+    // Obtenha o ID do carrinho de compras do serviço CartService
+    const shoppingCartId = this.cartService.getShoppingCartId();
+
+    // Adicione o produto ao carrinho usando o serviço ProductService (API)
+    this.productService.addProductToCart(item.id, quantity, shoppingCartId).subscribe(() => {
+      console.log('Produto adicionado ao carrinho (API)');
+    });
+
     item.quantity = 0; // Zera a quantidade do item após adicioná-lo ao carrinho
   }
 
