d396e08c9a5bc0855d1e8dfd1115adb5d67eb437
Gustavo Reimche <gustavoreimche@hotmail.com>
2023-05-10 14:27:21 -0300

    update image upload

--- src/app/register-product/register-product.component.ts
+++ src/app/register-product/register-product.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Item } from '../item-list/item.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-register-product',
@@ -38,24 +39,38 @@ export class RegisterProductComponent implements OnInit {
   }
 
   createProduct(): void {
-    if (this.newProduct.name && this.newProduct.description && this.newProduct.unit_price) {
-      this.productService.createProduct(this.newProduct).subscribe((response) => {
-        console.log('Produto criado com sucesso!');
-        this.newProduct = {
-          id: '',
-          name: '',
-          description: '',
-          unit_price: 0,
-          product_url: '',
-          quantity: 0,
-          quantityCart: 0,
-          editingName: false,
-          editingDescription: false,
-          editingUnitPrice: false,
-        };
+    if (
+      this.newProduct.name &&
+      this.newProduct.description &&
+      this.newProduct.unit_price
+    ) {
+      this.saveProduct().subscribe((response) => {
+        const productId = response.id;
+        console.log('ID do produto:', productId);
+        if (this.selectedFile) {
+          this.formData.append(
+            'image',
+            this.selectedFile,
+            this.selectedFile.name
+          );
+          console.log('FormData:');
+          this.formData.forEach((value, key) => {
+            console.log(`${key}:`, value);
+          });
+          this.productService
+            .uploadFile(productId, this.formData)
+            .subscribe((response) => {
+              this.newProduct.product_url = response.url;
+              console.log('Arquivo enviado com sucesso!', response);
+            });
+        }
       });
     } else {
       alert('Preencha todos os campos obrigatórios');
     }
+  }
+
+  saveProduct(): Observable<Item> {
+    return this.productService.createProduct(this.newProduct);
   }
 }
