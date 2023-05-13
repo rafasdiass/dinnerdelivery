7fe96af434ff4275ec8fe01ced2e4d2ae5d72cfd
Felipe Silva <127339296+iamfelipeS@users.noreply.github.com>
2023-05-13 12:13:42 -0300

    Deleted duplicate SRC directory

--- src/app/src/assets/img/ecommerce-main/scripts.js
+++
@@ -1,5 +0,0 @@
-let filtro = document.createElement('input');
-filtro.setAttribute('type', 'text');
-filtro.setAttribute('id', 'filtro');
-filtro.setAttribute('placeholder', 'Digite algo para filtrar..');
-container2.appendChild(filtro);
